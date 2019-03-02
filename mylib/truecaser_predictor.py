999443a3beee919b64b62523aed2abb1c58451ae
Stephen Mayhew <mayhew2@illinois.edu>
2019-03-02 12:03:32 -0600

    Added a batch predict method which is MUCH faster

--- mylib/truecaser_predictor.py
+++ mylib/truecaser_predictor.py
@@ -1,5 +1,5 @@
 from overrides import overrides
-
+from typing import List
 from allennlp.common.util import JsonDict
 from allennlp.data import DatasetReader, Instance
 from allennlp.data.tokenizers.word_splitter import JustSpacesWordSplitter
@@ -23,12 +23,22 @@ class TruecaserPredictor(Predictor):
         js = {"sentence" : sent}
         return self.predict_instance(self._json_to_instance(js))
 
+    @overrides
     def predict_instance(self, sent: Instance) -> JsonDict:
         output = super().predict_instance(sent)
         #output["chars"] = sent["tokens"]
         output["words"] = sent["tokens"].tokens
         return output
 
+    @overrides
+    def predict_batch_instance(self, sents: List[Instance]) -> List[JsonDict]:
+        outputs = super().predict_batch_instance(sents)
+        for i,sent in enumerate(sents):
+            #output["chars"] = sent["tokens"]
+            outputs[i]["words"] = sent["tokens"].tokens
+        return outputs
+
+    
     def load_line(self, line: str) -> JsonDict:
         """
         This will usually be overridden with use_dataset_reader = True on the command line.
